3742c067cc61c47943afe7902c39c3e87076e073
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-23 02:44:51 -0700

    first commit (automated command)

--- printNumber.c
+++ printNumber.c
@@ -1,46 +1,64 @@
 #include"main.h"
-void num_print_helper(int n, int *bytes)
+#include "main.h"
+
+int highest_power(int num)
 {
-	int numLen, divisor;
-	if (n == -2147483648)
-	{
-		*bytes += _putchar('-');
-		return;
-	}
-
-	if (n < 0)
-	{
-		*bytes += _putchar('-');
-		n = -n;
-	}
-	numLen = number_len(n);
-
-	divisor = _power(10, numLen - 1);
-	while (divisor)
-	{
-		int x = (int)(n / divisor);
-
-		*bytes += _putchar((x % 10) + '0');
-		divisor /= 10;
-	}
+    int power = 1;
+    while (num / (power * 10) > 0)
+        power *= 10;
+    return (power);
 }
 
-/**
- * print_number - function that print number
- * without using stdio lib
- *
- * @n: type int
- *
- * Return: number
- */
+void num_print_helper(int n, int *bytes)
+{
+    int  divisor;
+
+    if (n < 0)
+    {
+        *bytes += _putchar('-');
+        n = -n;
+    }
+
+    divisor = highest_power(n);
+    while (divisor)
+    {
+        int x = (int)(n / divisor);
+
+        *bytes += _putchar((x % 10) + '0');
+        divisor /= 10;
+    }
+}
+
 int print_number(va_list arg)
 {
-	int bytes;
-	int n;
+    int bytes;
+    int n;
 
-	n = va_arg(arg, int);
-	bytes = 0;
-	num_print_helper(n, &bytes);
+    n = va_arg(arg, int);
+    bytes = 0;
 
-	return (bytes);
+    if (n == 0) {
+        _putchar('0');
+        return 1;
+    }
+    else if (n == -2147483648) {
+        _putchar('-');
+        _putchar('2');
+        _putchar('1');
+        _putchar('4');
+        _putchar('7');
+        _putchar('4');
+        _putchar('8');
+        _putchar('3');
+        _putchar('6');
+        _putchar('4');
+        _putchar('8');
+        return 11;
+    }
+    else 
+    {
+        num_print_helper(n, &bytes);
+    }
+
+    return bytes;
 }
