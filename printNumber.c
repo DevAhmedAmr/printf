20486fa994c694e87189eb95e89598b372acf13b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-23 03:30:13 -0700

    first commit (automated command)

--- printNumber.c
+++ printNumber.c
@@ -1,69 +1,77 @@
-#include"main.h"
 #include "main.h"
-
 int highest_power(int num)
 {
-    int power = 1;
-    while (num / (power * 10) > 0)
-        power *= 10;
-    return (power);
+	int power = 1;
+	while (num / (power * 10) > 0)
+		power *= 10;
+	return (power);
 }
 
 void num_print_helper(int n, int *bytes)
 {
-    int  divisor;
+	int divisor;
 
-    if (n < 0)
-    {
-        *bytes += 1;
+	if (n < 0)
+	{
+		*bytes += 1;
+		_putchar('-');
+		n = -n;
+	}
 
-	_putchar('-');
+	divisor = highest_power(n);
 
-        n = -n;
-    }
-
-    divisor = highest_power(n);
-
-    while (divisor)
-    {
-        int x = (n / divisor);
-
-        *bytes += 1;
-	_putchar((x % 10) + '0');
-	divisor /= 10;
-    }
+	while (divisor)
+	{
+		int x = (n / divisor);
+		*bytes += 1;
+		_putchar((x % 10) + '0');
+		divisor /= 10;
+	}
 }
 
 int print_number(va_list arg)
 {
-    int bytes;
-    int n;
+	int bytes;
+	int n;
 
-    n = va_arg(arg, int);
+	n = va_arg(arg, int);
+	bytes = 0;
 
-    bytes = 0;
-
-    if (n == 0) 
-    {
-        _putchar('0');
-        return 1;
-    }
-    else if (n == -2147483648) {
-        _putchar('-');
-        _putchar('2');
-        _putchar('1');
-        _putchar('4');
-        _putchar('7');
-        _putchar('4');
-        _putchar('8');
-        _putchar('3');
-        _putchar('6');
-        _putchar('4');
-        _putchar('8');
-        return 11;
-    }
-    else 
-	    num_print_helper(n, &bytes);
-
-    return bytes;
+	if (n >= 0 && n <= 9)
+	{
+		_putchar(n + '0');
+		return 1;
+	}
+	else if (n < 0 && n > -10)
+	{
+		_putchar('-');
+		_putchar(-n + '0');
+		return 2;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return 1;
+	}
+	else if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('8');
+		return 11;
+	}
+	else
+	{
+		num_print_helper(n, &bytes);
+	}
+	
+	return bytes;
 }
