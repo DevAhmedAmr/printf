d28bbb228721f832dc2cd6b39217d14d1dee6761
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-22 21:13:00 -0700

    first commit (automated command)

--- printNumber.c
+++ printNumber.c
@@ -1,34 +1,30 @@
-#include "main.h"
- unsigned long int  highest_power(int num)
+#include"main.h"
+unsigned long int highest_power(int num)
 {
-	unsigned long int power = 1;
-		while (num / (power * 10) > 0)
-				power *= 10;
-	return (power);
+    unsigned long int power = 1;
+    while (num / (power * 10) > 0)
+        power *= 10;
+    return (power);
 }
 
-void splitNum(int num,int *byets)
+void splitNum(int num, int *bytes)
 {
-		unsigned long int power = 0 , digit = 0;
-		power = highest_power(num);
-		while (power > 0)
-		{
-		digit = num / power;
-		num %= power;
-		power /= 10;
-		_putchar(digit + 48);
-		*byets += 1;
-
-		}
+    unsigned long int power = 0, digit = 0;
+    if (num < 0) {
+        _putchar('-');
+        *bytes += 1;
+        num = -num;
+    }
+    power = highest_power(num);
+    while (power > 0)
+    {
+        digit = num / power;
+        num %= power;
+        power /= 10;
+        _putchar(digit + '0');
+        *bytes += 1;
+    }
 }
-/**
- * print_number - function that print number
- * without using stdio lib
- *
- * @n: type int
- *
- * Return: number
- */
 
 int print_number(va_list arg)
 {
@@ -36,28 +32,34 @@ int print_number(va_list arg)
 
     n = va_arg(arg, int);
 
-    if (n >= 0 && n <= 9)
-    {
-        _putchar(n + '0');
-        bytes++;
+    if (n == 0) {
+        _putchar('0');
+        return 1;
     }
-    else if (n < 0 && n >= -9)
-    {
+    else if (n == INT_MIN) {
         _putchar('-');
-        _putchar((-n) + '0');
-        bytes += 2;
+        _putchar('2');
+        _putchar('1');
+        _putchar('4');
+        _putchar('7');
+        _putchar('4');
+        _putchar('8');
+        _putchar('3');
+        _putchar('6');
+        _putchar('4');
+        _putchar('8');
+        return 11;
     }
-    else if (n > 9)
+    else if (n < 0)
     {
         splitNum(n, &bytes);
     }
-    else if (n < -9)
+    else if (n > 0)
     {
-        int nn;
-        _putchar('-');
-        bytes++;
-        nn = n * -1;
-        splitNum(nn, &bytes);
+        splitNum(n, &bytes);
     }
-    return (bytes);
+    else {
+        return 0;
+    }
+    return bytes;
 }
