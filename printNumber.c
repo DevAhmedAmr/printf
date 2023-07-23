aa5251274b4c64a2a78e5fb4368872ea7a8b34d9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-22 22:00:53 -0700

    updated with recusion (automated command)

--- printNumber.c
+++ printNumber.c
@@ -1,65 +1,49 @@
 #include"main.h"
-unsigned long int highest_power(int num)
+void splitNum(long int n, int *bytes)
 {
-    unsigned long int power = 1;
-    while (num / (power * 10) > 0)
-        power *= 10;
-    return (power);
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		(*bytes)++;
+		n = -n;
+	}
 
-void splitNum(int num, int *bytes)
-{
-    unsigned long int power = 0, digit = 0;
-    if (num < 0) {
-        _putchar('-');
-        *bytes += 1;
-        num = -num;
-    }
-    power = highest_power(num);
-    while (power > 0)
-    {
-        digit = num / power;
-        num %= power;
-        power /= 10;
-        _putchar(digit + '0');
-        *bytes += 1;
-    }
-}
+	if (n / 10)
+		splitNum(n / 10, bytes);
 
+	_putchar(n % 10 + '0');
+	(*bytes)++;
+}
 int print_number(va_list arg)
 {
-    int bytes = 0, n;
+	int bytes;
+	long int n;
 
-    n = va_arg(arg, int);
+	n = va_arg(arg, long int);
+	bytes = 0;
+	if (n >= 0 && n <= 9)
+	{
+		_putchar(n + '0');
+	}
 
-    if (n == 0) {
-        _putchar('0');
-        return 1;
-    }
-    else if (n == -2147483648) {
-        _putchar('-');
-        _putchar('2');
-        _putchar('1');
-        _putchar('4');
-        _putchar('7');
-        _putchar('4');
-        _putchar('8');
-        _putchar('3');
-        _putchar('6');
-        _putchar('4');
-        _putchar('8');
-        return 11;
-    }
-    else if (n < 0)
-    {
-        splitNum(n, &bytes);
-    }
-    else if (n > 0)
-    {
-        splitNum(n, &bytes);
-    }
-    else {
-        return 0;
-    }
-    return bytes;
+	else if (n < 0 && n > -9)
+	{	
+		_putchar('-');
+		_putchar(n + '0');
+		bytes += 2;
+	}
+
+	if (n > 9)
+		splitNum(n, &bytes);
+
+	if (n < -9)
+	{
+
+		_putchar('-');
+		bytes++;
+
+		splitNum(-(n), &bytes);
+	}
+	return (bytes);
 }
+
