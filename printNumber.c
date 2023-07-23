3133c2c61dc885f43cc25c87c7dfc76a635c4399
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-23 03:34:18 -0700

    first commit (automated command)

--- printNumber.c
+++ printNumber.c
@@ -1,77 +1,66 @@
 #include "main.h"
-int highest_power(int num)
+#include"main.h"
+unsigned long int highest_power(int num)
 {
-	int power = 1;
-	while (num / (power * 10) > 0)
-		power *= 10;
-	return (power);
+    unsigned long int power = 1;
+    while (num / (power * 10) > 0)
+        power *= 10;
+    return (power);
 }
 
-void num_print_helper(int n, int *bytes)
+void splitNum(int num, int *bytes)
 {
-	int divisor;
-
-	if (n < 0)
-	{
-		*bytes += 1;
-		_putchar('-');
-		n = -n;
-	}
-
-	divisor = highest_power(n);
-
-	while (divisor)
-	{
-		int x = (n / divisor);
-		*bytes += 1;
-		_putchar((x % 10) + '0');
-		divisor /= 10;
-	}
+    unsigned long int power = 0, digit = 0;
+    if (num < 0) {
+        _putchar('-');
+        *bytes += 1;
+        num = -num;
+    }
+    power = highest_power(num);
+    while (power > 0)
+    {
+        digit = num / power;
+        num %= power;
+        power /= 10;
+        _putchar(digit + '0');
+        *bytes += 1;
+    }
 }
 
 int print_number(va_list arg)
 {
-	int bytes;
-	int n;
+    int bytes = 0, n;
 
-	n = va_arg(arg, int);
-	bytes = 0;
+    n = va_arg(arg, int);
 
-	if (n >= 0 && n <= 9)
-	{
-		_putchar(n + '0');
-		return 1;
-	}
-	else if (n < 0 && n > -10)
-	{
-		_putchar('-');
-		_putchar(-n + '0');
-		return 2;
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return 1;
-	}
-	else if (n == -2147483648)
-	{
-		_putchar('-');
-		_putchar('2');
-		_putchar('1');
-		_putchar('4');
-		_putchar('7');
-		_putchar('4');
-		_putchar('8');
-		_putchar('3');
-		_putchar('6');
-		_putchar('4');
-		_putchar('8');
-		return 11;
-	}
-	else
-	{
-		num_print_helper(n, &bytes);
-	}
-	
-	return bytes;
+    if (n == 0) {
+        _putchar('0');
+        return 1;
+    }
+    else if (n == -2147483648) {
+        _putchar('-');
+        _putchar('2');
+        _putchar('1');
+        _putchar('4');
+        _putchar('7');
+        _putchar('4');
+        _putchar('8');
+        _putchar('3');
+        _putchar('6');
+        _putchar('4');
+        _putchar('8');
+        return 11;
+    }
+    else if (n < 0)
+    {
+        splitNum(n, &bytes);
+    }
+    else if (n > 0)
+    {
+        splitNum(n, &bytes);
+    }
+    else {
+        return 0;
+    }
+    return bytes;
 }
