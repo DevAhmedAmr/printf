fd465fcbb4fd10938482ba802abf81ca0ad59b80
MoedCode <mohamed71291@gmail.com>
2023-07-23 22:32:40 -0700

    No Retreat, No Surrender (automated command)

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(void)
 	int len;
 	int len2;
 	unsigned int m = 98;
+
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	_printf("Length:[%d, %i]\n", len, len);
