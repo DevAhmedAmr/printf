cc0ec416d24391d407fc4ca2018262b7ccf8e932
MoedCode <mohamed71291@gmail.com>
2023-07-23 23:59:13 -0700

    No Retreat, No Surrender (automated command)

--- binary.c
+++ binary.c
@@ -1,37 +1,56 @@
 #include "main.h"
+/**
+ * Reverse - Reverse and print given string
+ * @binBuff: string to be reversed
+ * @return (int) number of chars that printed
+ */
+int Reverse(char *binBuff)
+{
+	int midPoint = 0, ln, i;
+	char tmp;
 
+	i = _strlen(binBuff);
+	ln = i;
+	midPoint = ln / 2;
+	for (i = 0; i < midPoint; i++)
+	{
+		tmp = binBuff[i];
+		binBuff[i] = binBuff[(ln - 1) - i];
+		binBuff[(ln - 1) - i] = tmp;
+	}
+	i = 0;
+	while ( binBuff[i] != '\0')
+	{
+		_putchar(binBuff[i]);
+		i++;
+	}
+	return (i);
+}
 /**
  * * print_binary - print a binary for given number
  * @arg: argument list for number to print its binary
  * @return (int) number of chrcter of buffer of binary
  */
-int print_binary(va_list arg){
-	int j, len, temp;
-	unsigned int num;
-	int binary_arr[1000];
-
-	num = va_arg(args, unsigned int);
-
-	len = 0;
-	while (num > 0)
+int print_binary(va_list arg)
+{
+	char binBuff[1000];
+	unsigned int i = 0, n, buffLn;
+	n = va_arg(arg, unsigned int);
+	printf("binNum [%u]", n);
+	do
 	{
-		temp = 0;
-		temp = num % 2;
-		binary_arr[len] = temp + 48;
-		num /= 2;
-		len++;
+		if (n % 2 == 0)
+			binBuff[i] = '0';
+		else
+			binBuff[i] = '1';
+		n/= 2;
+		i++;
 	}
+	while(n != 0);
+	binBuff[i] = '\0';
+	printf("binBuff [%s]\n",binBuff);
+	buffLn = Reverse(binBuff);
 
-	j = len - 1;
-	while (j >= 0)
-	{
-		*buffer.box = binary_arr[j];
-		buffer.box++;
-		buffer.size++;
-		j--;
-	}
-
-	buffer.box--;
-	return (buffer);
+	return (buffLn);
 }
 
