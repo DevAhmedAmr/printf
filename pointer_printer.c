4955482a0135f1f7dc66cbc68117a341a684b657
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-25 13:09:06 +0300

    applying fixes (automated command)

--- pointer_printer.c
+++ pointer_printer.c
@@ -66,7 +66,7 @@ int ptr_printer(va_list args)
 	for (i = 0; i < 18; i++)
 		buffer[i] = '0';
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 18; i++)
 	{
 		convertToHexLetter_ptr(&bytes, &num, temp);
 
