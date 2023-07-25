db6f85f1e4baaea7448addc1870c08f838fa1aba
MoedCode <mohamed71291@gmail.com>
2023-07-24 22:34:54 -0700

    (automated command)

--- hexaCap.c
+++ hexaCap.c
@@ -1,5 +1,41 @@
 #include "main.h"
-
+/**
+ * convertToHexLetter_X_capital - a helper function that converts
+ * the number that are bigger than 9 to hex decimal with capital
+ * letters
+ *
+ * parameters:
+ * @bytes: number of printed bytes
+ * @num: number to be converted
+ * @buffer: buffer to save the sub number
+ */
+void convertToHexLetter_X_capitall(int *bytes, unsigned int *num, char *buffer)
+{
+	switch (*num % 16)
+	{
+	case 10:
+		buffer[*bytes] = 'A';
+		break;
+	case 11:
+		buffer[*bytes] = 'B';
+		break;
+	case 12:
+		buffer[*bytes] = 'C';
+		break;
+	case 13:
+		buffer[*bytes] = 'D';
+		break;
+	case 14:
+		buffer[*bytes] = 'E';
+		break;
+	case 15:
+		buffer[*bytes] = 'F';
+		break;
+	default:
+		buffer[*bytes] = *num % 16 + '0';
+		break;
+	}
+}
 /**
  * hex_capital - function that convert number for base 10 to base 16
  * with a capital letters
@@ -9,7 +45,6 @@
  *
  * Return: the number of bytes of the printed number
  */
-
 int hex_capitalA(unsigned int num)
 {
 
@@ -20,7 +55,7 @@ int hex_capitalA(unsigned int num)
 	while (1)
 	{
 		breakLoop = (num / 16 == 0) ? 1 : 0;
-		convertToHexLetter_X_capital(&bytes, &num, buffer);
+		convertToHexLetter_X_capitall(&bytes, &num, buffer);
 
 		num = num / 16;
 
