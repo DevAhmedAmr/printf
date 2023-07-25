1ef182f2e554e46c4b9cfc5c772206976c969e7e
MoedCode <mohamed71291@gmail.com>
2023-07-24 22:31:02 -0700

    (automated command)

--- hexaCap.c
+++ hexaCap.c
@@ -1,41 +1,5 @@
 #include "main.h"
-/**
- * convertToHexLetter_X_capital - a helper function that converts
- * the number that are bigger than 9 to hex decimal with capital
- * letters
- *
- * parameters:
- * @bytes: number of printed bytes
- * @num: number to be converted
- * @buffer: buffer to save the sub number
- */
-void convertToHexLetter_X_capital(int *bytes, unsigned int *num, char *buffer)
-{
-	switch (*num % 16)
-	{
-	case 10:
-		buffer[*bytes] = 'A';
-		break;
-	case 11:
-		buffer[*bytes] = 'B';
-		break;
-	case 12:
-		buffer[*bytes] = 'C';
-		break;
-	case 13:
-		buffer[*bytes] = 'D';
-		break;
-	case 14:
-		buffer[*bytes] = 'E';
-		break;
-	case 15:
-		buffer[*bytes] = 'F';
-		break;
-	default:
-		buffer[*bytes] = *num % 16 + '0';
-		break;
-	}
-}
+
 /**
  * hex_capital - function that convert number for base 10 to base 16
  * with a capital letters
