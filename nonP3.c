3729e3af09a0a3761a8aad7af70fa6f8250da65e
MoedCode <mohamed71291@gmail.com>
2023-07-25 18:01:07 -0700

    (automated command)

--- nonP3.c
+++ nonP3.c
@@ -1,31 +1,45 @@
 #include "main.h"
 
-
-int nonPrintAscii(va_list arg) {
+/**
+ * nonPrintAscii - print string repalceing nonprint
+ * chars it with its hexadecimal value
+ *
+ * @arg: string to print
+ *
+ * Return: (int) number of charcter printed
+ */
+int nonPrintAscii(va_list arg)
+{
 	int i, j, byets;
-	char* upStr;
+	char *upStr;
 	HexChar hexCharA[] = {
-	{0, '0'},{1, '1'},{2, '2'},{3, '3'},{4, '4'},
-	{5, '5'},{6, '6'},{7, '7'},{8, '8'},{9, '9'},
-	{10, 'A'},{11, 'B'},{12, 'C'},{13, 'D'},{14, 'E'},{15, 'F'}};
+	{0, '0'}, {1, '1'}, {2, '2'}, {3, '3'}, {4, '4'},
+	{5, '5'}, {6, '6'}, {7, '7'}, {8, '8'}, {9, '9'},
+	{10, 'A'}, {11, 'B'}, {12, 'C'}, {13, 'D'}, {14, 'E'}, {15, 'F'}};
 
 upStr = va_arg(arg, char*);
 	if (upStr == NULL)
 		upStr = "(null)";
-	for (i = 0; upStr[i] !='\0'; i++) {
-		if ((upStr[i] > 0 && upStr[i] < 32) || upStr[i] >= 127) {
+	for (i = 0; upStr[i] != '\0'; i++)
+	{
+		if ((upStr[i] > 0 && upStr[i] < 32) || upStr[i] >= 127)
+		{
 			_putchar('\\');
 			_putchar('x');
 			_putchar('0');
 			byets += 3;
-			for (j = 0; j <= 15; j++) {
-				if (upStr[i] == hexCharA[j].N) {
+			for (j = 0; j <= 15; j++)
+			{
+				if (upStr[i] == hexCharA[j].N)
+				{
 					_putchar(hexCharA[j].C);
 					byets++;
 					break;
 				}
 			}
-		} else {
+		}
+		else
+		{
 			_putchar(upStr[i]);
 			byets++;
 		}
--- prtRevStr.c
+++ prtRevStr.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
+/**
+ * prtRevStr - print string in reversed order
+ *
+ * @args: sring to print
+ *
+ * Return: (int) number of printed charcter
+ */
+
 int prtRevStr(va_list args)
 {
 	int byets = 0, i = 0, len = 0;
-	char* S;
+	char *S;
 
 S = va_arg(args, char *);
 	if (S == NULL)
 		return (0);
 	len = _strlen(S);
 
-	for (i = len - 1; i >= 0; i-- )
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(S[i]);
 		byets++;
